398e2ef0681fd2609fcd1ade01aa7ccaccef49b8
Loukson <123784461+Loukson@users.noreply.github.com>
2025-11-08 04:58:58 +0000

    Add files via upload

---
+++ config.js
@@ -0,0 +1,6 @@
+const global = {
+    owner: [7327737440], // ganti jadi id mu
+    botToken: "8205427930:AAGvEcnEazoDY6X1g3svktRfBa1-RCoek6E", //isi make token bot mu
+}
+
+module.exports = global;
